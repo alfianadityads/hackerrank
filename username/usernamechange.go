6aeb0e9c94a098e9b03537a66cf7439c4548265c
alfianadityads <alfianadityadwi@gmail.com>
2023-08-17 23:35:46 +0700

    make possibleChange function

--- username/usernamechange.go
+++ username/usernamechange.go
@@ -1,1 +1,25 @@
 package username
+
+import (
+	"sort"
+	"strings"
+)
+
+func possibleChange(usernames []string) []string {
+
+	results := []string{}
+
+	for _, username := range usernames {
+		chars := strings.Split(username, "")
+		sort.Strings(chars)
+		rearranged := strings.Join(chars, "")
+
+		if rearranged != username {
+			results = append(results, "YES")
+		} else {
+			results = append(results, "NO")
+		}
+	}
+
+	return results
+}
